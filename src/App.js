e929f4a94003891c296053841e872d92d65bcd14
Iblal <81813762+Iblal@users.noreply.github.com>
2022-01-07 13:25:32 +0000

    Added Coinpage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } 
+from "react-router-dom";
+import {
+  makeStyles
+} from "@material-ui/core";
 import './App.css';
-import Header from './Components/Header';
+import Header from './components/Header';
+import Homepage from './Pages/Homepage';
+import Coinpage from './Pages/Coinpage';
+
+
+ const useStyles = makeStyles(() => ({
+   App: {
+     backgroundColor: "#14161a",
+     color: "white",
+     minHeight: "100vh",
+   },
+ }));
+
 
 function App() {
+  const classes = useStyles();
+
   return (
     <BrowserRouter>
-    <div>
-    <Header />
-
-    </div>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage/>} exact />
+          <Route path="/coins/:id" element={<Coinpage/>} exact />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
